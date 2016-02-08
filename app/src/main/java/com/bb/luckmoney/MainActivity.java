1e65e3fe986637fab3f1b75b94bf17277544afb5
superman566 <ghe@lakeheadu.ca>
2016-02-07 23:52:49 -0500

    set app name to center

--- app/src/main/java/com/bb/luckmoney/MainActivity.java
+++ app/src/main/java/com/bb/luckmoney/MainActivity.java
@@ -1,20 +1,27 @@
 package com.bb.luckmoney;
 
 import android.content.Intent;
-import android.provider.Settings;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.provider.Settings;
+import android.support.v7.app.ActionBar;
+import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
     private Button setting_button;
     private Intent accessibleIntent = new Intent(Settings.ACTION_ACCESSIBILITY_SETTINGS);
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        setting_button = (Button)findViewById(R.id.setting_button);
+
+        //create actionbar_layout in order to override actionbar
+        getSupportActionBar().setDisplayOptions(ActionBar.DISPLAY_SHOW_CUSTOM);
+        getSupportActionBar().setCustomView(R.layout.actionbar_layout);
+
+        setting_button = (Button) findViewById(R.id.setting_button);
         setting_button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
